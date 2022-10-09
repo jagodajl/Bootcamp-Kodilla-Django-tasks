220fbeebdb68a4eaf8cc0856e36fa8605cf00ae4
Jagoda Jeczmien-Lazur <jagoda.jeczmienlazur@gmail.com>
2022-10-09 19:05:29 +0200

    templates_excercises_commit

--- maths/views.py
+++ maths/views.py
@@ -1,29 +1,33 @@
-from django.shortcuts import render
-
-# Create your views here.
 # maths/views.py
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
 
 def math(request):
-   return HttpResponse("Tu będzie matma")
+    return HttpResponse("Tu będzie matma")
+
 
 def add(request, a, b):
-   a, b = int(a), int(b)
-   return HttpResponse(a + b)
+    a, b = int(a), int(b)
+    wynik = a + b
+    t = loader.get_template("maths/operation.html")
+    c = {"a": a, "b": b, "operacja": "+", "wynik": wynik, "title": "dodawanie"}
+    return HttpResponse(t.render(c))
+
 
 def sub(request, a, b):
-   a, b = int(a), int(b)
-   return HttpResponse(a - b)
+    a, b = int(a), int(b)
+    return HttpResponse(a - b)
+
 
 def mul(request, a, b):
-   a, b = int(a), int(b)
-   return HttpResponse(a * b)
+    a, b = int(a), int(b)
+    return HttpResponse(a * b)
+
 
 def div(request, a, b):
-   a, b = int(a), int(b)
-   if b == 0:
-       return HttpResponse("Nie dziel przez 0")
-   return HttpResponse(a / b)
+    a, b = int(a), int(b)
+    if b == 0:
+        return HttpResponse("Nie dziel przez 0")
+    return HttpResponse(a / b)
