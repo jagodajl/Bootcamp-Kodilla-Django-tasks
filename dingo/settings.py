cbc2f5be3339bee7b9d65de9b5535c610b47da95
Jagoda Jeczmien-Lazur <jagoda.jeczmienlazur@gmail.com>
2022-10-12 21:09:14 +0200

    Posts app added.

--- dingo/settings.py
+++ dingo/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'maths'
+    'maths',
+    'greetings',
+    'posts',
 ]
 
 MIDDLEWARE = [
---
+++ posts/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from posts.views import posts_list, post_details, authors_list, author_details, homepage
+
+app_name = "posts"
+urlpatterns = [
+    path('', homepage),
+    path('authors/', authors_list, name="authors_list"),
+    path('authors/<int:id>', author_details, name="author_details"),
+    path('posts/', posts_list, name="posts_list"),
+    path('posts/<int:id>', post_details, name="post_details"),
+]
